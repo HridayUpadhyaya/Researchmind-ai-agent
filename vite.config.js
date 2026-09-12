835e27057e956f501d9f12f301a226e238ddaeec
Hriday Nishant Upadhyaya <hridayupad@gmail.com>
2026-09-12 11:43:30 +0530

    Refactor vite.config.js for better readability

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 
 export default defineConfig({
-  plugins: [react()],
+
+  plugins: [
+    react()
+  ],
+
+
   server: {
+
     port: 5173,
+
+    proxy: {
+
+      "/api": {
+
+        target:
+          "http://localhost:8000",
+
+        changeOrigin:
+          true,
+
+        secure:
+          false,
+
+      },
+
+    },
+
   },
-})
+
+
+  build: {
+
+    sourcemap:
+      true,
+
+    minify:
+      "esbuild",
+
+  },
+
+
+  envPrefix:
+    "VITE_",
+
+});
